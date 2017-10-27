32a5aebd22087b81ca91fbb9773bbe3e587da362
Xong Xiong <xxiong3153@gmail.com>
2017-10-27 00:39:36 -0500

    hard mode finished

--- client.js
+++ client.js
@@ -5,19 +5,21 @@ myApp.controller('GuessWhoController', function(){
     var people = [{ name: 'Xong', github: 'xongxiong', show: true}, 
                   {name: 'Chris', github: 'christopher-black', show: true}, 
                   {name: 'Kris', github: 'kdszafranski', show: true}];
+
+    var hiddenPeople = [];
+
     guess.people = people;
     guess.newName = '';
     guess.newGithub = '';
     guess.submit = function() {
-        people.push({name: guess.newName, github: guess.newGithub});
+        people.push({name: guess.newName, github: guess.newGithub, show:true});
         console.log(people);
     }
-
-    guess.rand = 0;
     
     guess.initRand = function () {
-        guess.rand = Math.floor(Math.random() * people.length)
-        guess.dispName = people[guess.rand].name;
+        rand = Math.floor(Math.random() * people.length)
+        guess.dispName = people[rand].name;
+        
     }
 
     guess.thisPerson = function(person) {
@@ -34,7 +36,17 @@ myApp.controller('GuessWhoController', function(){
     function thisPersonShow(person) {
         var i= guess.people.indexOf(person);
         console.log(i);
-        people[i].show = false;
+        people.splice(i, 1);
+        hiddenPeople.push(person);
+        console.log(hiddenPeople);
+        if (people.length === 0) {
+            for (var j = 0; j < hiddenPeople.length; j++) {
+                people.push(hiddenPeople[j]);
+            }
+            hiddenPeople = [];
+            console.log(hiddenPeople);
+            console.log(people);
+        }
     }
 });
 
