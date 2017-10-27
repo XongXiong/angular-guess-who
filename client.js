e09723bebacd3688bf49b4c80b72dc6aefdbb2ae
Xong Xiong <xxiong3153@gmail.com>
2017-10-27 01:34:10 -0500

    added more people

--- client.js
+++ client.js
@@ -2,22 +2,23 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('GuessWhoController', function(){
     var guess = this;
-    var people = [{ name: 'Xong', github: 'xongxiong', show: true}, 
-                  {name: 'Chris', github: 'christopher-black', show: true}, 
-                  {name: 'Kris', github: 'kdszafranski', show: true}];
-
+    // Starting Array with people shown
+    var peopleArr = people;
+    console.log(peopleArr);
+    // Hidden Array that contains person object when person is clicked on
     var hiddenPeople = [];
 
-    guess.people = people;
+    guess.people = peopleArr;
     guess.newName = '';
     guess.newGithub = '';
+    
     guess.submit = function() {
-        people.push({name: guess.newName, github: guess.newGithub, show:true});
+        peopleArr.push({name: guess.newName, github: guess.newGithub, show:true});
     }
     
     guess.initRand = function () {
-        rand = Math.floor(Math.random() * people.length)
-        guess.dispName = people[rand].name;
+        rand = Math.floor(Math.random() * peopleArr.length)
+        guess.dispName = peopleArr[rand].name;
         
     }
 
@@ -33,13 +34,14 @@ myApp.controller('GuessWhoController', function(){
 
     function thisPersonShow(person) {
         var i= guess.people.indexOf(person);
-        people.splice(i, 1);
+        peopleArr.splice(i, 1);
         hiddenPeople.push(person);
-        if (people.length === 0) {
+        if (peopleArr.length === 0) {
             for (var j = 0; j < hiddenPeople.length; j++) {
-                people.push(hiddenPeople[j]);
+                peopleArr.push(hiddenPeople[j]);
             }
             hiddenPeople = [];
+            alert('Congratulations!')
         }
     }
 });
---
+++ data.js
@@ -0,0 +1,87 @@
+var people = [
+    {
+        name: 'Zack',
+        github: 'zackstout',
+        show: true
+    },
+    {
+        name: 'Josh',
+        github: 'joshnothum',
+        show: true
+    },
+    {
+        name: 'Marta',
+        github: 'martajopp',
+        show: true
+    },
+    {
+        name: 'Blaze',
+        github: 'blaze90809',
+        show: true
+    },
+    {
+        name: 'Sarah',
+        github: 'sarahharrington',
+        show: true
+    },
+    {
+        name: 'Chris',
+        github: 'christopher-black',
+        show: true
+    },
+    {
+        name: 'Aaron',
+        github: 'bozeman42',
+        show: true
+    },
+    {
+        name: 'Alex',
+        github: 'alexanderbliss',
+        show: true
+    },
+    {
+        name: 'David',
+        github: 'dbeaudway',
+        show: true
+    },
+    {
+        name: 'Holly',
+        github: 'hollytuhake',
+        show: true
+    },
+    {
+        name: 'Emma',
+        github: 'emmastout01',
+        show: true
+    },
+    {
+        name: 'Xong',
+        github: 'xongxiong',
+        show: true
+    },
+    {
+        name: 'Laura',
+        github: 'goetsit',
+        show: true
+    },
+    {
+        name: 'Jaffa',
+        github: 'jaharonov',
+        show: true
+    },
+    {
+        name: 'Emi',
+        github: 'emimbchen',
+        show: true
+    },
+    {
+        name: 'Elvis',
+        github: 'elvishang',
+        show: true
+    },
+    {
+        name: 'Hanna',
+        github: 'bemeo1088',
+        show: true
+    }
+];
